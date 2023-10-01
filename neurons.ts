0d00802c80b55df77aab698e93091fc47bc79984
Dave Mandelin <dmandelin@gmail.com>
2023-10-01 09:18:53 -0700

    Add simple tests of the perceptron learning majority functions (easily) and parity (not at all).

--- neurons.ts
+++ neurons.ts
@@ -16,6 +16,23 @@ class Layer {
     apply(I: number[]): number[] {
         return mul(this.W, I);
     }
+
+    learn(I: number[], e: number[], alpha: number) {
+        for (let i = 0; i < this.W.length; ++i) {
+            const Wi = this.W[i];
+            for (let j = 0; j < I.length; ++j) {
+                Wi[j] -= alpha * e[i] * I[j];
+            }
+        }
+    }
+}
+
+function sub(a: number[], b: number[]): number[] {
+    const v: number[] = [];
+    for (let i = 0; i < a.length; ++i) {
+        v.push(a[i] - b[i]);
+    }
+    return v;
 }
 
 function dot(a: number[], b: number[]): number {
@@ -38,8 +55,68 @@ function log(a: any) {
     console.log(a);
 }
 
-const layer = new Layer(6, 1);
-console.log(layer.W);
-const I = [1, 1, 0, 1, 0, 1];
-const O = layer.apply(I);
-console.log(O);
+function testLayer(genExample: () => [number[], number[]]) {
+    const batches = 10;
+    const batchSize = 100;
+
+    const m = 3;
+    const layer = new Layer(m + 1, 1);
+    const alpha = 0.01;
+
+    for (let bi = 0; bi < batches; ++bi) {
+        let hits = 0;
+        let loss = 0;
+        console.log('* Batch', bi+1);
+        for (let ii = 0; ii < batchSize; ++ii) {
+            const [I, EO] = genExample();
+
+            // Get result from layer.
+            const O = layer.apply(I);
+            //console.log(`  - ${I} -> ${O[0].toFixed(4)} (EO = ${EO[0]})`);
+            
+            // Compute error and update loss.
+            const e = sub(O, EO);
+            loss += e[0] * e[0];
+            if (Math.sign(O[0]) == Math.sign(EO[0])) ++hits;
+
+            // Learn.
+            layer.learn(I, e, alpha);
+        }
+        console.log(`  Acc = ${(hits/batchSize).toFixed(4)}, Loss = ${loss}`);
+    }
+    console.log('weights: ', layer.W);
+}
+
+function testMajority() {
+    const m = 3;
+
+    testLayer(() => {
+        const I = [1];
+        let total = 0;
+        for (let j = 0; j < m; ++j) {
+            const v = Math.random() < 0.5 ? 0 : 1;
+            I.push(v);
+            total += v;
+        }
+        const EO = [total * 2 > m ? 1 : -1];
+        return [I, EO];
+    });
+}
+
+function testParity() {
+    const m = 3;
+
+    testLayer(() => {
+        const I = [1];
+        let total = 0;
+        for (let j = 0; j < m; ++j) {
+            const v = Math.random() < 0.5 ? 0 : 1;
+            I.push(v);
+            total += v;
+        }
+        const EO = [total % 2];
+        return [I, EO];
+    });
+}
+
+testParity();
