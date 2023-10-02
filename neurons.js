4c5fff5ee9963d41dfd8f9813e8559cab97f7769
Dave Mandelin <dmandelin@gmail.com>
2023-10-01 17:18:03 -0700

    Initial visualization of units and link weights.

--- neurons.js
+++ neurons.js
@@ -1,8 +1,101 @@
+class Visualizer {
+    layer;
+    // idea:
+    // - show box per unit
+    // - set input and see values, errors, and learning in each layer
+    // - visualize coefficients as line color/width/etc
+    // - click on a unit to see its coefficients
+    unitSide = 48;
+    unitGap = 24;
+    layerGap = this.unitSide * 3;
+    svg;
+    inputs = [];
+    outputs = [];
+    links = [];
+    linkLabels = [];
+    constructor(layer) {
+        this.layer = layer;
+        const height = (this.unitGap + this.unitSide) * layer.m - this.unitGap + 2;
+        this.svg = appendToId(createSvg('svg', {
+            'width': this.unitSide * 2 + this.layerGap + 2,
+            'height': height,
+        }), 'vis');
+        // Input layer
+        let x = 1;
+        let y = 1;
+        for (let i = 0; i < layer.m; ++i) {
+            this.inputs.push(appendTo(createSvg('rect', {
+                'x': x, 'y': y, 'width': this.unitSide, 'height': this.unitSide,
+                'stroke': '#222', 'fill': '#dee',
+            }), this.svg));
+            y += this.unitSide + this.unitGap;
+        }
+        // Output layer
+        let olh = (this.unitGap + this.unitSide) * layer.n - this.unitGap;
+        x += this.unitSide + this.layerGap;
+        y = (height - olh) / 2;
+        for (let i = 0; i < layer.n; ++i) {
+            this.outputs.push(appendTo(createSvg('rect', {
+                'x': x, 'y': y, 'width': this.unitSide, 'height': this.unitSide,
+                'stroke': '#222', 'fill': '#eed',
+            }), this.svg));
+            y += this.unitSide + this.unitGap;
+        }
+        // Links
+        for (let i = 0; i < layer.n; ++i) {
+            const outputLinks = [];
+            const outputLinkLabels = [];
+            for (let j = 0; j < layer.m; ++j) {
+                const x1 = Number(this.inputs[j].getAttribute('x')) + this.unitSide;
+                const y1 = Number(this.inputs[j].getAttribute('y')) + this.unitSide / 2;
+                const x2 = Number(this.outputs[i].getAttribute('x'));
+                const y2 = Number(this.outputs[i].getAttribute('y')) + this.unitSide / 2;
+                outputLinks.push(appendTo(createSvg('line', { x1, y1, x2, y2, stroke: '#666' }), this.svg));
+                appendTo(createSvg('rect', {
+                    x: x1 + this.unitSide * 0.25 - 2,
+                    y: y1 - 13,
+                    width: 32,
+                    height: 14,
+                    fill: '#fff',
+                }), this.svg);
+                outputLinkLabels.push(appendTo(createSvg('text', {
+                    x: x1 + this.unitSide * 0.25,
+                    y: y1,
+                    'font-family': 'sans-serif',
+                    'font-size': '12px',
+                }, layer.W[i][j].toFixed(2)), this.svg));
+            }
+            this.links.push(outputLinks);
+            this.linkLabels.push(outputLinkLabels);
+        }
+    }
+}
+function createSvg(tagName, attrs = {}, innerText) {
+    const e = document.createElementNS('http://www.w3.org/2000/svg', tagName);
+    for (const [k, v] of Object.entries(attrs)) {
+        e.setAttribute(k, String(v));
+    }
+    if (innerText)
+        e.innerHTML = innerText;
+    return e;
+}
+function appendTo(child, parent) {
+    parent.appendChild(child);
+    return child;
+}
+function appendToId(child, parentId) {
+    document.getElementById(parentId).appendChild(child);
+    return child;
+}
 class Layer {
+    m;
+    n;
     // Row-major order: W[i] is the set of weights to dot with inputs
     // to get output i.
     W = [];
     constructor(m, n) {
+        this.m = m;
+        this.n = n;
         for (let i = 0; i < n; ++i) {
             const Wi = [];
             for (let j = 0; j < m; ++j) {
@@ -102,4 +195,5 @@ function testParity() {
         return [I, EO];
     });
 }
+const vis = new Visualizer(new Layer(4, 1));
 testParity();
--- neurons.ts
+++ neurons.ts
@@ -1,9 +1,108 @@
+class Visualizer {
+    // idea:
+    // - show box per unit
+    // - set input and see values, errors, and learning in each layer
+    // - visualize coefficients as line color/width/etc
+    // - click on a unit to see its coefficients
+
+    protected readonly unitSide = 48;
+    protected readonly unitGap = 24;
+    protected readonly layerGap = this.unitSide * 3;
+
+    protected readonly svg: SVGElement;
+    protected readonly inputs: SVGElement[] = [];
+    protected readonly outputs: SVGElement[] = [];
+    protected readonly links: SVGElement[][] = [];
+    protected readonly linkLabels: SVGElement[][] = [];
+
+    constructor(protected layer: Layer) {
+        const height = (this.unitGap + this.unitSide) * layer.m - this.unitGap + 2;
+        this.svg = appendToId(createSvg('svg', {
+            'width': this.unitSide * 2 + this.layerGap + 2,
+            'height': height,
+        }), 'vis');
+
+        // Input layer
+        let x = 1;
+        let y = 1;
+        for (let i = 0; i < layer.m; ++i) {
+            this.inputs.push(appendTo(createSvg('rect', {
+                'x': x, 'y': y, 'width': this.unitSide, 'height': this.unitSide,
+                'stroke': '#222', 'fill': '#dee',
+            }), this.svg));
+            y += this.unitSide + this.unitGap;
+        }
+
+        // Output layer
+        let olh = (this.unitGap + this.unitSide) * layer.n - this.unitGap;
+        x += this.unitSide + this.layerGap;
+        y = (height - olh) / 2;
+        for (let i = 0; i < layer.n; ++i) {
+            this.outputs.push(appendTo(createSvg('rect', {
+                'x': x, 'y': y, 'width': this.unitSide, 'height': this.unitSide,
+                'stroke': '#222', 'fill': '#eed',
+            }), this.svg));
+            y += this.unitSide + this.unitGap;
+        }
+
+        // Links
+        for (let i = 0; i < layer.n; ++i) {
+            const outputLinks = [];
+            const outputLinkLabels = [];
+            for (let j = 0; j < layer.m; ++j) {
+                const x1 = Number(this.inputs[j].getAttribute('x')) + this.unitSide;
+                const y1 = Number(this.inputs[j].getAttribute('y')) + this.unitSide / 2;
+                const x2 = Number(this.outputs[i].getAttribute('x'));
+                const y2 = Number(this.outputs[i].getAttribute('y')) + this.unitSide / 2;
+                outputLinks.push(appendTo(createSvg('line', {x1, y1, x2, y2, stroke: '#666'}), this.svg));
+                appendTo(createSvg('rect', {
+                    x: x1 + this.unitSide * 0.25 - 2,
+                    y: y1 - 13,
+                    width: 32,
+                    height: 14,
+                    fill: '#fff',
+                }), this.svg);
+                outputLinkLabels.push(appendTo(createSvg('text', {
+                    x: x1 + this.unitSide * 0.25,
+                    y: y1,
+                    'font-family': 'sans-serif',
+                    'font-size': '12px',
+                }, layer.W[i][j].toFixed(2)), this.svg));
+            }
+            this.links.push(outputLinks);
+            this.linkLabels.push(outputLinkLabels);
+        }
+    }
+}
+
+function createSvg(tagName: string, attrs: SVGAttributes = {}, innerText?: string): SVGElement {
+    const e = document.createElementNS('http://www.w3.org/2000/svg', tagName);
+    for (const [k, v] of Object.entries(attrs)) {
+        e.setAttribute(k, String(v));
+    }
+    if (innerText) e.innerHTML = innerText;
+    return e;
+}
+
+function appendTo<T extends HTMLElement|SVGElement>(child: T, parent: HTMLElement|SVGElement): T {
+    parent.appendChild(child);
+    return child;
+}
+
+function appendToId<T extends HTMLElement|SVGElement>(child: T, parentId: string): T {
+    document.getElementById(parentId).appendChild(child);
+    return child;
+}
+
+type SVGAttributes = {
+};
+
 class Layer {
     // Row-major order: W[i] is the set of weights to dot with inputs
     // to get output i.
     readonly W: number[][] = [];
 
-    constructor(m: number, n: number) {
+    constructor(public readonly m: number, public readonly n: number) {
         for (let i = 0; i < n; ++i) {
             const Wi = [];
             for (let j = 0; j < m; ++j) {
@@ -118,5 +217,7 @@ function testParity() {
         return [I, EO];
     });
 }
+
+const vis = new Visualizer(new Layer(4, 1));
 
 testParity();
